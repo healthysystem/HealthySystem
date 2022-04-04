ca1dd53777dea1af56f3d8f119e0a85a072a6dcc
healthysystem <102437510+healthysystem@users.noreply.github.com>
2022-04-04 14:58:29 -0500

    HealthySystem
    Subir repositorio

---
+++ js/init.js
@@ -0,0 +1,24 @@
+// @ts-ignore
+//configuraCION OMAR
+firebase.initializeApp({
+  apiKey: "AIzaSyAnj1v66EJuXIYQRBrr-NEsDBsE-fuTgAk",
+  authDomain: "bxxs-iot.firebaseapp.com",
+  projectId: "bxxs-iot",
+  storageBucket: "bxxs-iot.appspot.com",
+  messagingSenderId: "478914869660",
+  appId: "1:478914869660:web:86f97f8108883028e67059"
+});
+
+// configuracion propia healthy
+// firebase.initializeApp({
+//   apiKey: "AIzaSyBP1OKe3z_56MOKGW1VAcsklf2twPSp-d4",
+//   authDomain: "healthysystem701-44e05.firebaseapp.com",
+//   projectId: "healthysystem701-44e05",
+//   storageBucket: "healthysystem701-44e05.appspot.com",
+//   messagingSenderId: "911650150836",
+//   appId: "1:911650150836:web:6ef7177ea52472e9183ae1"
+  
+// });
+
+
+
